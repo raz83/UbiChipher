45ff4f227ffeb0a7877db66000823536de603b71
DaeZohn <abieerasmus10@hotmail.com>
2020-05-20 10:33:35 +0100

    Added Bitcoin Demon and Canvas for EnrollmentTestUtil

---
+++ UbiChipher.Infrastructure.Blockchain.DevHost/BitDeamon.bat
@@ -0,0 +1,2 @@
+cd C:\Program Files\Bitcoin\daemon
+bitcoind -conf=C:\Users\User\AppData\Roaming\Bitcoin\bitcoin.conf -testnet
---
+++ UbiChipher.Infrastructure.Blockchain.DevHost/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UbiChipher.Infrastructure.Blockchain.DevHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Run the demonic Bitcoin magic Internet money.
+            System.Diagnostics.Process.Start("BitDeamon.bat");
+        }
+    }
+}
